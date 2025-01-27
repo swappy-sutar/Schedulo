5b89515ec376ae3b10021a7cbaf48c021fffe713
swapnil <sutarswapnil322@gmail.com>
2025-01-27 23:58:49 +0530

    Enhance user model with additional fields: phoneNumber, address, employeeId, salary, and searchedCandidates

--- Backend/Models/user.model.js
+++ Backend/Models/user.model.js
@@ -1,36 +1,59 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["Admin", "HR"],
-    default: "HR",
-  },
-  timeslots: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Timeslot",
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      lowercase: true,
     },
-  ],
-}, { timestamps: true });
+    lastname: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "HR"],
+      default: "HR",
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    employeeId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    salary: {
+      type: Number,
+    },
+    searchedCandidates: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Candidate",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
