3c5786cff834ea9bb0a8bc19b20cb1d05ba8b493
swapnil <sutarswapnil322@gmail.com>
2025-01-28 00:00:03 +0530

    Add change password route to user routes

--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createUser, loginUser } from "../Controllers/user.controller.js";
+import { changePassword, createUser, loginUser } from "../Controllers/user.controller.js";
+import {isAdmin,isHr,auth} from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/login", loginUser);
+router.post("/change-password",auth, changePassword);
+
 
 
 export default router;
