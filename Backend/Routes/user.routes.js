a6f0eeaa7f7d0b42f03e930f4f6e27a98e42ecd6
swapnil <sutarswapnil322@gmail.com>
2025-02-02 23:25:45 +0530

    Add password reset functionality to user routes

--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { changePassword, createUser, loginUser } from "../Controllers/user.controller.js";
-import {isAdmin,isHr,auth} from "../middlewares/auth.middleware.js"
+import {isAdmin,isHr,auth} from "../Middlewares/auth.middleware.js"
+import { resetPassword, resetPasswordToken } from "../Controllers/resetPassword.Controller.js";
 
 const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/login", loginUser);
 router.post("/change-password",auth, changePassword);
-
+router.post("/reset-password-token", resetPasswordToken);
+router.post("/reset-password", resetPassword);
 
 
 export default router;
