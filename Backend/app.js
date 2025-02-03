de45eecb4442ca71bf5e0c86f47784b168237d83
swapnil <sutarswapnil322@gmail.com>
2025-02-03 22:46:09 +0530

    Update CORS configuration to include PUT method

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:3000"],
-  methods: ["POST", "GET", "DELETE", "OPTIONS"],
+  methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 };
 
